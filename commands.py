3a75ba6471f378515be39a7ddf9ace054187d7b7
SnippleRifsky <42582187+SnippleRifsky@users.noreply.github.com>
2023-08-09 01:06:43 -0400

    output formatting

--- commands.py
+++ commands.py
@@ -15,9 +15,7 @@ async def list(ctx):
     await ctx.send("Fetching player list...")
 
     # Find the last occurrence of "players online" in the logs
-    last_players_online_command = (
-        "grep 'players online' logs/latest.log* | tail -n 1"
-    )
+    last_players_online_command = "grep 'players online' logs/latest.log* | tail -n 1"
     last_players_online_output = shell.run(last_players_online_command, hide=True)
 
     last_players_online_line = last_players_online_output.stdout.strip()
@@ -26,19 +24,16 @@ async def list(ctx):
     last_timestamp = last_players_online_line.split("[")[1].split("]")[0]
 
     # Search for lines with the same timestamp in the logs
-    lines_with_timestamp_command = (
-        f"grep '\\[{last_timestamp}\\]' logs/latest.log*"
-    )
+    lines_with_timestamp_command = f"grep '\\[{last_timestamp}\\]' logs/latest.log*"
     lines_with_timestamp_output = shell.run(lines_with_timestamp_command, hide=True)
 
     lines_with_timestamp = lines_with_timestamp_output.stdout.strip().split("\n")
 
-    # Send the filtered lines to Discord
-    for line in lines_with_timestamp:
-        await ctx.send("```\n" + line + "\n```")
+    # Combine the lines into a single string
+    combined_lines = "\n".join(lines_with_timestamp)
 
-
-
+    # Send the combined lines as a single message to Discord
+    await ctx.send("```\n" + combined_lines + "\n```")
 
 
 @client.command()
